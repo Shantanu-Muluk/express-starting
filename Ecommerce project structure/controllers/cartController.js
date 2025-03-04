0ebb56195932bd8689fdc5ece2f620cee8d5e0e7
Shantanu-Muluk <shantanumuluk9878@gmail.com>
2025-03-04 18:55:20 +0530

    Adding controllers

---
+++ Ecommerce project structure/controllers/cartController.js
@@ -0,0 +1,10 @@
+// /controllers/cartController.js
+
+exports.getCartForUser = (req, res) => {
+    res.send(`Fetching cart for user with ID: ${req.params.userId}`);
+  };
+  
+  exports.addProductToCart = (req, res) => {
+    res.send(`Adding product to cart for user with ID: ${req.params.userId}`);
+  };
+  
